4ceb9d7af85fef8ff905d81f39b767e66b176722
tCu0n9 <tacuong3000@gmail.com>
2023-07-24 11:37:01 +0700

    Update nmap_txc.py file

--- nmap_txc.py
+++ nmap_txc.py
@@ -1,23 +1,6 @@
 import nmap
 import csv
 
-scanner = nmap.PortScanner()
-
-print('\t\tWelcome to Nmap')
-print('<---------------------------------------------->')
-
-ip_addr = input('Press IP: ')
-print('==> IP target: ', ip_addr)
-type(ip_addr)
-
-port = input('Press ports: ')
-if not port.isdigit() or int(port) <= 0 or int(port) > 65535:
-    print('Invalid port. Using default port range: 1-1024')
-    port = '1-1024'
-else:
-    print('==> Ports target: ',port)
-type(port)
-
 def results_format(results):
     lines = results.strip().split('\n')
     
@@ -29,44 +12,66 @@ def results_format(results):
         print('-' * 30)
         for i in range(len(header)):
             print(f"{header[i]}: {row[i]}")
+            
+def scanner_nmap(ip_addr,port,types):
+    scanner = nmap.PortScanner()
 
-types = input('''\nSelect type scan:
-                1. SYN ACK 
-                2. UDP 
-                3. Comprehensive
+    if types == '1':
+        print('\nNmap Version: ', scanner.nmap_version())
+        scanner.scan(ip_addr,port,'-v -sS')
+        print(scanner.scaninfo())
+        print('IP status: ', scanner[ip_addr].state())
+        print(scanner[ip_addr].all_protocols())
+        print('Open ports: ',scanner[ip_addr]['tcp'].keys())
+        results = scanner.csv().replace('\r','')
+        
+        results_format(results)
+
+    elif types == '2':
+        print('\nNmap Version: ',scanner.nmap_version())
+        scanner.scan(ip_addr,port,'-v -sU')
+        print(scanner.scaninfo())
+        print('IP status: ',scanner[ip_addr].state())
+        print(scanner[ip_addr].all_protocols())
+        print('Open ports: ',scanner[ip_addr]['udp'].keys())
+        results = scanner.csv().replace('\r','')
+        
+        results_format(results)
+
+    elif types == '3':
+        print('\nNmap Version: ', scanner.nmap_version())
+        scanner.scan(ip_addr,port,'-v -sS -sC -sV -A -O')
+        print(scanner.scaninfo())
+        ipsta = print('IP status: ', scanner[ip_addr].state())
+        print(scanner[ip_addr].all_protocols())
+        print('Open ports: ',scanner[ip_addr]['tcp'].keys())
+        results = scanner.csv().replace('\r','')
+        
+        results_format(results)
+    elif types >= '4':
+        print('Your type not available!! Please, try again!!')
+
+
+if __name__ == "__main__":
+    print('\t\tWelcome to Nmap')
+    print('<---------------------------------------------->')
+
+    ip_addr = input('Press IP: ')
+    print('==> IP target: ', ip_addr)
+    type(ip_addr)
+
+    port = input('Press ports: ')
+    if not port.isdigit() or int(port) <= 0 or int(port) > 65535:
+        print('==> Invalid port. Using default port range: 1-1024')
+        port = '1-1024'
+    else:
+        print('==> Ports target: ',port)
+    type(port)
+    
+    types = input('''\nSelect type scan:
+                    1. SYN ACK 
+                    2. UDP 
+                    3. Comprehensive
 Select: ''')
-
-if types == '1':
-    print('\nNmap Version: ', scanner.nmap_version())
-    scanner.scan(ip_addr,port,'-v -sS')
-    print(scanner.scaninfo())
-    print('IP status: ', scanner[ip_addr].state())
-    print(scanner[ip_addr].all_protocols())
-    print('Open ports: ',scanner[ip_addr]['tcp'].keys())
-    results = scanner.csv().replace('\r','')
     
-    results_format(results)
-
-elif types == '2':
-    print('\nNmap Version: ',scanner.nmap_version())
-    scanner.scan(ip_addr,port,'-v -sU')
-    print(scanner.scaninfo())
-    print('IP status: ',scanner[ip_addr].state())
-    print(scanner[ip_addr].all_protocols())
-    print('Open ports: ',scanner[ip_addr]['udp'].keys())
-    results = scanner.csv().replace('\r','')
-    
-    results_format(results)
-
-elif types == '3':
-    print('\nNmap Version: ', scanner.nmap_version())
-    scanner.scan(ip_addr,port,'-v -sS -sC -sV -A -O')
-    print(scanner.scaninfo())
-    ipsta = print('IP status: ', scanner[ip_addr].state())
-    print(scanner[ip_addr].all_protocols())
-    print('Open ports: ',scanner[ip_addr]['tcp'].keys())
-    results = scanner.csv().replace('\r','')
-    
-    results_format(results)
-elif types >= '4':
-    print('Your type not available!! Please, try again!!')
+    scanner_nmap(ip_addr,port,types)
